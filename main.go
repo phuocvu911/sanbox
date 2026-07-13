d3168c613fa82b495af9ab0afe9b5f8e2cbae7ed
Hoang Phuoc Vu <iamphuocvux@gmail.com>
2026-07-13 12:57:24 +0300

    Refactor database operations and error handling in main.go

--- main.go
+++ main.go
@@ -2,53 +2,98 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 
+	"github.com/mattn/go-sqlite3"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	//open db
-	db, _ := sql.Open("sqlite3", "./data.db")
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close() //
 	//create table
 	query := `
 		CREATE TABLE IF NOT EXISTS users (
-			id INTEGER PRIMARY KEY,
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
 			name TEXT NOT NULL,
 			email TEXT UNIQUE
 		);
 	`
-	_, err := db.Exec(query)
+	_, err = db.Exec(query)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	//insert data
-	_, err = db.Exec(
-		"INSERT INTO users(id, name, email) VALUES(?, ?, ?)",
-		1, "Alice", "aliceinwonderland@gmail.com",
-	)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	addUser(db, "alice", "aliceemial@gmail.com")
+	addUser(db, "beta", "betal@gmail.com")
+	addUser(db, "cena", "cenal@gmail.com")
+	addUser(db, "dela", "deal@gmail.com")
 	//read data
-	var (
-		id    int
-		name  string
-		email string
+	fmt.Println(listUsers(db))
+	fmt.Println(getUserByEmail(db, "deal@gmail.com"))
+}
+
+func addUser(db *sql.DB, name, email string) error {
+	_, err := db.Exec(
+		"INSERT INTO users(name, email) VALUES(?, ?)",
+		name, email,
 	)
 
-	err = db.QueryRow(
-		"SELECT id, name, email FROM users WHERE id = ?",
-		1,
-	).Scan(&id, &name, &email)
-
 	if err != nil {
-		log.Fatal(err)
+		var sqlErr sqlite3.Error
+		if errors.As(err, &sqlErr) &&
+			sqlErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return fmt.Errorf("UNIQUE contraint violation")
+		} else {
+			return err
+		}
+	}
+	return nil
+}
+
+type User struct {
+	ID    int
+	Name  string
+	Email string
+}
+
+func listUsers(db *sql.DB) ([]User, error) {
+	res := []User{}
+	rows, err := db.Query(`SELECT id, name, email FROM users`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close() //
+	for rows.Next() {
+		var u User
+		err = rows.Scan(&u.ID, &u.Name, &u.Email)
+		if err != nil {
+			return res, err
+		}
+		res = append(res, u)
 	}
 
-	fmt.Println(id, name, email)
+	if err = rows.Err(); err != nil {
+		return res, err
+	}
+	return res, nil
+}
+
+func getUserByEmail(db *sql.DB, email string) (*User, error) {
+	var u User
+	row := db.QueryRow(`SELECT id, name, email FROM users WHERE email = ?`, email)
+	if err := row.Scan(&u.ID, &u.Name, &u.Email); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &u, nil
 }
